29721dad171d7360af90e8fc21410aa4d29f82a0
Connor Cummings and LaTaevia Berry <lataevia@>
2016-04-05 15:28:54 -0700

    added constructor function and prototype methods, html, and css

---
+++ js/script.js
@@ -0,0 +1,33 @@
+function BankAccount(name, initialDeposit, balance){
+this.name = name;
+this.initialDeposit = initialDeposit;
+this.balance = balance;
+}
+
+BankAccount.prototype.withdrawl= function(){
+this.balance -= withdrawl;
+}
+BankAccount.prototype.deposit = function(){}
+this.balance += deposit;
+}
+
+
+$(document).ready(function(){
+  $('form#sign-up').submit(function(event){
+    event.preventDefault();
+    alert("clicked");
+    var inputtedName = $("input#account-name");
+    var inputtedDeposit = $("input#initial-deposit");
+    var balance = initialDeposit;
+    var newPerson = BankAccout(inputtedName, inputtedDeposit, balance);
+    console.log(newPerson);
+    // var initwithdrawlEquat = []
+
+
+
+
+
+  });
+});
+var person = $("").val
+var
